b51e4c3b270b762c3c37b357c9cca7d1515e6a7c
Jim <jimpalmeri12@gmail.com>
2022-01-03 16:47:14 -0500

    First commit

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import discord
+import asyncio
+
+# Load discord token
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+# When the bot is ready
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# When a message is received
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if not message.guild:
+        print(message.content)
+        await message.channel.send("Please buy more G packs to fund my yacht")
+        return
+
+    print("Hey, it's me, Kondo!")
+
+client.run(TOKEN)
