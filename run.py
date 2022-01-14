8398edefdff8721fabafc720ea0c05a5544b9087
Jim <jimpalmeri12@gmail.com>
2022-01-14 10:45:38 -0500

    Rename automatically, also rename # names

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 import discord
 import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+sekaicord_id = 636538718387306528
+test_server_id = 907674756684333187
 
 # Load discord token
 load_dotenv()
@@ -15,13 +19,14 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    load_cron()
 
 # When a message is received
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
     if not message.guild:
         if message.author.id == 319599477424128001:
             await message.channel.send("If you don't buy a G pack I'm making a 33 that's worse than Roku")
@@ -45,9 +50,19 @@ async def rename_all_users(guild):
     users = 0
     for i in range(len(guild.members)):
         user = guild.members[i]
-        if (user.nick is None and user.name[0] == "!") or (not (user.nick is None) and user.nick[0] == "!"):
+        banned = ["!", "#"]
+        if (user.nick is None and user.name[0] in banned) or (not (user.nick is None) and user.nick[0] in banned):
             users += 1
             await user.edit(nick="Hardcore Kondo Oshi")
     return users
+
+async def rename_all():
+    guild = client.get_guild(sekaicord_id)
+    await rename_all_users(guild)
+
+def load_cron():
+    sched = AsyncIOScheduler(timezone='EST')
+    sched.start()
+    sched.add_job(rename_all, trigger='cron', minute=15)
 
 client.run(TOKEN)
